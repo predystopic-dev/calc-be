b07b9630684199ca7e759b2c8a995c322d3cd813
azure <azure@localhost.localdomain>
2024-09-13 18:40:07 +0530

    Updated Prompt for PEMDAS

--- apps/calculator/utils.py
+++ apps/calculator/utils.py
@@ -38,11 +38,19 @@ def analyze_image(img: Image, dict_of_vars: dict):
     model = genai.GenerativeModel(model_name="gemini-1.5-flash")
     dict_of_vars_str = json.dumps(dict_of_vars, ensure_ascii=False)
     prompt = (
+        f"You have been given an image with some mathematical expressions, equations, or graphical problems, and you need to solve them. "
+        f"Note: Use PEMDAS rule for solving mathematical expressions, PEMDAS mean Priority Order for Parentheses, Exponents, Multiplication and Division (from left to right), Addition and Subtraction (from left to right. Parantheses have highest priority, followed by Exponents, Multiplication and Division, and lastly Addition and Subtraction. "
+        f"for example,"
+        f"Q. 2+3*4"
+        f"(3*4) => 12, 2+12 = 14."
+        f"Q. 2+3+5*4-8/2"
+        f"5*4 => 20, 8/2 => 4, 2+3 => 5, 5+20 => 25, 25-4 => 21."
         f"YOU CAN HAVE FIVE TYPES OF EQUATIONS/EXPRESSIONS IN THIS IMAGE, AND ONLY ONE CASE SHALL APPLY EVERY TIME: "
+        f"Following are the cases: "
         f"1. Simple mathematical expressions like 2+2, 3*4, 5/6, 7-8 etc. : This case, need to solve and return the answer in format as LIST OF ONE DICT [{{expr: given expression, result: calculated answer}}]"
         f"2. Set of Equations like x^2 + 2x + 1 = 0, 3y + 4x = 0, 5x^2 + 6y + 7 = 12 etc. : This case, need to solve for given variable, and format should be COMMA SEPARATED LIST OF DICTS, dict 1 as {{expr: x, result: 2, assign: True}}, and dict 2 as {{expr: y, result: 5, assign: True}} This example is assuming x was calculated as 2, and y as 5. Have as many dicts as vars"
         f"3. Assigning values to variables like x = 4, y = 5, z = 6 etc. : This case, you need to assign values to variables and return another key in dict called {{assign: True}}, and keep variable as expr and value as result in original dictionary. RETURN AS LIST OF DICTS"
-        f"4. Analyzing Graphical Math problems, like word problems represented in graphical form, for eg, cars colliding, trigonometric problem, pythagoras theorem problems, runs wagon wheel problems etc., these will have a drawing representing some scenario and accompanying information with image, PAY CLOSE ATTENTION TO DIFFERENT COLORS FOR THESE PROBLEMS. You need to return the answer in format as LIST OF ONE DICT [{{expr: given expression, result: calculated answer}}]"
+        f"4. Analyzing Graphical Math problems, that is word problems represented in drawing form, for eg, cars colliding, trigonometric problems, problems on pythagoras theorem problems, adding runs from cricket wagon wheel etc., these will have a drawing representing some scenario and accompanying information with image, PAY CLOSE ATTENTION TO DIFFERENT COLORS FOR THESE PROBLEMS. You need to return the answer in format as LIST OF ONE DICT [{{expr: given expression, result: calculated answer}}]"
         f"5. Detecting Abstract Concepts that drawing might show, like love, hate, jealousy, patriotism, or a historic reference to war, invention, discovery, quote etc. USE SAME FORMAT AS OTHERS TO RETURN ANSWER, expression will be explanation of drawing, and result will be the abstract concept"
         f"Analyze the equation or expression in this image and return the answer acccording to given rules: "
         f"make sure to use extra backslashes for escape characters like \\f->\\\\f, \\n->\\\\n etc. "
